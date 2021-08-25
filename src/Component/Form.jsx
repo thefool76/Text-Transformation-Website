49785e10d86630ca095248a1e81504920c54d628
thefool76 <sumitmishra1135@gmail.com>
2021-08-25 22:29:13 +0530

    added some text preview

--- src/Component/Form.jsx
+++ src/Component/Form.jsx
@@ -5,27 +5,32 @@ export default function Form(props) {
     console.log("Hello" + text);
     let newText = text.toUpperCase();
     setText(newText);
-  }
+  };
   const handleOnChange = (event) => {
     console.log("get it");
     setText(event.target.value);
-  }
+  };
   const [text, setText] = useState("");
   return (
     <>
-      <h1>{props.head} </h1>
-      <div className="mb-3">
-        <textarea
-          id="myBox"
-          value={text}
-          rows="8"
-          onChange={handleOnChange}
-          className="form-control"
-      
-        ></textarea>
+      <div className="coantainer">
+        <h1>{props.head} </h1>
+        <div className="mb-3">
+          <textarea
+            id="myBox"
+            value={text}
+            rows="8"
+            onChange={handleOnChange}
+            className="form-control"
+          ></textarea>
+        </div>
         <button className="btn btn-primary my-2" onClick={handleUpClick}>
           Convert TO Uppercase
         </button>
+      </div>
+      <div className="container my-3">
+        <h1>Text Summary</h1>
+        <p>{text.split(" ").length} Words & {text.length} Characters</p>
       </div>
     </>
   );
